1669f39c0072c42b507ac90dea6f9ae7a24cf3e0
Pranoy Kundu <pranoy1998k@gmail.com>
2023-07-05 16:24:34 +0530

    delete the aws ami once testing is done

--- tests/packer_test.go
+++ tests/packer_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/packer"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,9 +13,9 @@ func TestPackerHelloWorldExample(t *testing.T) {
 	packerOptions := &packer.Options{
 		Template: "../ami-template.json",
 	}
-
+    region := "eu-north-1"
 	amiID := packer.BuildArtifact(t, packerOptions)
-	
+	defer aws.DeleteAmi(t , region, amiID)
 	assert.NotEmpty(t, amiID, "AMI ID should not be an empty string.")
 }
 
@@ -28,21 +29,18 @@ func TestPackerAMI(t *testing.T) {
     amiID := packer.BuildAmi(t, packerOptions)
 
     // Validate the AMI using Terratest AWS helpers
-    awsRegion := "your-aws-region"
-    awsInstanceType := "your-instance-type"
+    awsRegion := "eu-north-1"
+    awsInstanceType := "t3.micro"
 
     // Create an AWS client session
+    
     awsRegionOptions := &aws.GetRegionOptions{
         Profile: "your-aws-profile",
     }
-    awsClient := aws.NewAuthenticatedClient(t, awsRegion, awsRegionOptions)
+    awsClient := aws.NewAuthenticatedSession(t, awsRegion, awsRegionOptions)
 
     // Validate the AMI properties
-    amiTags := map[string]string{
-        "your-tag-key": "your-tag-value",
-    }
     ami := aws.GetAmiById(t, awsClient, amiID, awsRegion)
-    aws.AssertAmiHasTags(t, ami.ID, amiTags)
     aws.AssertAmiExists(t, awsClient, ami.ID, awsRegion)
     aws.AssertInstanceTypeForAmi(t, ami.ID, awsInstanceType, awsRegion)
 }
